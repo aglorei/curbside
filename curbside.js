5e2c2fe9b0cbb781b2b79c9e95f8e96bae3b8408
aglorei <aglorei88@gmail.com>
2017-10-25 01:57:26 +0000

    Asynchronize Curbside#search
    Still not quite there (secrets are out of order), but this enables us to
    traverse the entire tree relatively quickly.

--- curbside.js
+++ curbside.js
@@ -1,28 +1,74 @@
 const https = require('https')
 const hostname = 'challenge.curbside.com'
 
+const fetch = (path, headers = {}) => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: headers,
+      hostname: hostname,
+      method: 'GET',
+      path: '/' + path,
+      port: 443
+    }
+
+    const req = https.request(options, (res) => {
+      switch (res.statusCode) {
+        case 200:
+          res.on('data', (d) => {
+            resolve(JSON.parse(d.toString()))
+          })
+          break
+        case 429:
+          res.on('data', (d) => {
+            reject(JSON.parse(d.toString()))
+          })
+      }
+    })
+
+    req.on('error', (e) => {
+      reject(e)
+    })
+
+    req.end()
+  })
+}
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 class Session {
   constructor () {
     this._stack = []
-    this._message = ''
+    this._secret = ''
   }
 
-  get stack ()          { return this._stack }
-  set stack (node_id)   { this._stack.push(node_id) }
-  get message ()        { return this._message }
-  set message (message) { this._message += message }
+  get stack ()        { return this._stack }
+  set stack (node_id) { this._stack.push(node_id) }
+  get secret ()       { return this._secret }
+  set secret (secret) { this._secret += secret }
 
   async get_session () {
-    const data = await this.fetch('get-session')
+    const data = await fetch('get-session')
     this.session = data['session']
     this.expire_at = data['expire_at']
     this.headers = {Session: this.session}
-    this.search()
+    await this.search()
   }
 
   async get_node (node_id) {
-    await this.sleep(1500)
-    const data = await this.fetch(node_id, this.headers)
+    let data
+    let success = false
+
+    while (!success) {
+      try {
+        data = await fetch(node_id, this.headers)
+        success = true
+      } catch(e) {
+        const wait = Math.random() * 1000
+        await sleep(wait)
+      }
+    }
 
     Object.keys(data).map(key => {
       data[key.toLowerCase()] = data[key]
@@ -31,51 +77,24 @@ class Session {
     if (data.hasOwnProperty('next') && !Array.isArray(data['next'])) {
       data['next'] = Array(data['next'])
     }
+
     return data
   }
 
-  search (node_id = 'start') {
+  async search (node_id = 'start') {
     this.stack = node_id
+    const data = await this.get_node(node_id)
 
-    this.get_node(node_id)
-      .then(data => {
-        if (data.hasOwnProperty('message')) {
-          this.message = data['message']
-        }
-        if (data.hasOwnProperty('next')) {
-          data['next'].forEach(id => {
-            if (!this.stack.includes(id)) {
-              this.search(id)
-            }
-          })
-        }
-      })
-  }
+    if (data.hasOwnProperty('secret')) {
+      this.secret = data['secret']
+    }
 
-  fetch (path, headers = {}) {
-    return new Promise((resolve, reject) => {
-      const options = {
-        headers: headers,
-        hostname: hostname,
-        method: 'GET',
-        path: '/' + path,
-        port: 443
+    if (data.hasOwnProperty('next')) {
+      for (let id of data['next']) {
+        if (!this.stack.includes(id)) {
+          this.search(id)
+        }
       }
-
-      const req = https.request(options, (res) => {
-        res.on('data', (d) => {
-          console.log(JSON.parse(d.toString()))
-          resolve(JSON.parse(d.toString()))
-        })
-      })
-
-      req.on('error', (e) => {
-        reject(e)
-      })
-
-      req.end()
-    })
+    }
   }
-
-  sleep (ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
 }
