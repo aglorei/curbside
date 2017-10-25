466415be275c07ea043258f7d3268ebb662e8390
aglorei <aglorei88@gmail.com>
2017-10-25 07:04:20 +0000

    correct message! recursive promises implementation
    Getting both asynchronous requests and the correct ordering of secret
    letterings required a synchronous read of recursively fulfilled promises
    at each level of the tree

--- curbside.js
+++ curbside.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 const https = require('https')
 const hostname = 'challenge.curbside.com'
 
@@ -12,17 +14,19 @@ const fetch = (path, headers = {}) => {
     }
 
     const req = https.request(options, (res) => {
+      let fulfillment
       switch (res.statusCode) {
         case 200:
-          res.on('data', (d) => {
-            resolve(JSON.parse(d.toString()))
-          })
+          fulfillment = resolve
           break
-        case 429:
-          res.on('data', (d) => {
-            reject(JSON.parse(d.toString()))
-          })
+        default:
+          fulfillment = reject
       }
+
+      res.on('data', (d) => {
+        const data = JSON.parse(d.toString())
+        fulfillment(data)
+      })
     })
 
     req.on('error', (e) => {
@@ -45,15 +49,15 @@ class Session {
 
   get stack ()        { return this._stack }
   set stack (node_id) { this._stack.push(node_id) }
-  get secret ()       { return this._secret }
-  set secret (secret) { this._secret += secret }
 
   async get_session () {
     const data = await fetch('get-session')
     this.session = data['session']
     this.expire_at = data['expire_at']
     this.headers = {Session: this.session}
-    await this.search()
+    this.search().then(secret => {
+      console.log(secret)
+    })
   }
 
   async get_node (node_id) {
@@ -81,20 +85,35 @@ class Session {
     return data
   }
 
-  async search (node_id = 'start') {
-    this.stack = node_id
-    const data = await this.get_node(node_id)
-
-    if (data.hasOwnProperty('secret')) {
-      this.secret = data['secret']
-    }
-
-    if (data.hasOwnProperty('next')) {
-      for (let id of data['next']) {
-        if (!this.stack.includes(id)) {
-          this.search(id)
+  search (node_id = 'start', secret = '') {
+    return new Promise((resolve, reject) => {
+      this.stack = node_id
+      this.get_node(node_id).then(data => {
+        if (data.hasOwnProperty('secret')) {
+          resolve(data['secret'])
         }
-      }
-    }
+
+        let child_promises = []
+
+        if (data.hasOwnProperty('next')) {
+          for (let id of data['next']) {
+            if (!this.stack.includes(id)) {
+              child_promises.push(this.search(id, secret))
+            }
+          }
+        }
+
+        Promise.all(child_promises)
+          .then(letters => {
+            for (let letter of letters) {
+              secret += letter
+            }
+            resolve(secret)
+          })
+      })
+    })
   }
 }
+
+const sess = new Session()
+sess.get_session()
